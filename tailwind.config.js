44faf77a1d26761d99a09844edf91923b23c4b65
mohamed_magdy <mohamedmagdyabdelhamed99@gmail.com>
2023-11-12 13:33:35 +0200

    made some files and some tailwind to prepare the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        nav: "#9BBEC8",
+        page: "#164863",
+        card: "#DDF2FD",
+        "card-hover": "#427D9D",
+        "defult-text": "#DDF2FD",
+        "blue-accent": "#39A7FF",
+        "blue-accent-hover": "#87C4FF",
       },
     },
   },
   plugins: [],
-}
+};
